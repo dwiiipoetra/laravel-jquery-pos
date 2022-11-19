6255534773848df41fce1637412df8e242cfbebd
Dwi Waluyo Putranto <45709789+dwiiipoetra@users.noreply.github.com>
2022-11-19 19:20:57 +0700

    generate no faktur

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Orders;
 use App\Models\OrdersDetail;
 use App\Models\Products;
@@ -20,12 +21,18 @@ class OrdersController extends Controller
 
     public function store(Request $r){
         $order = new Orders();
-        // $tesid = Orders::latest()->id->get();
-        $order->no_faktur = "INV-000";
+        // generate no faktur
+        $count = Orders::count() + 1;//get count row then + 1
+        $row_number = '00'.$count;
+        $currentTime = Carbon::now()->toDateTimeString();
+        $conv_date = substr(str_replace('-','', $currentTime),0,8);
+        $no = $conv_date.$row_number;
+        $auto=substr($no,8);//001
+        $auto_number='INV-'.substr($no,0,8).str_repeat(0,(4-strlen($auto))).$auto;
+        $order->no_faktur = $auto_number;
         $order->consumer_name = $r->consumer_name;
         $order->user_id = Auth::user()->id;
         $order->save();
-        
         return redirect()->route('orders.index');
     }
 
